ef761bc8f04aa5a1ccc64095f4360cd144b20288
hworthX51@hotmail.com <you@example.com>
2018-02-15 19:04:27 -0500

    updated connection

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "MONGODB_URI: mongodb://heroku_gd8f4gl0:ttrv6b904r8vk28l9e8b5ee7he@ds239128.mlab.com:39128/heroku_gd8f4gl0",
   {
     useMongoClient: true
   }
